943611477cdfaec50b30c327a07a6b2730fa9f27
Pedrini <psmarnilag@gmail.com>
2024-10-22 23:57:28 -0400

    Subiendo el solucionario de la practica #3

--- app.py
+++ app.py
@@ -7,10 +7,19 @@ app.secret_key = 'clave_secreta'
 def index():
   if 'datos' not in session:
     session['datos'] =[]    #Inicializando para almacenar datos
-  return render_template('index.html', datos=session['datos'])
+  return render_template('index.html')
+
+@app.route('/lista')
+def lista():
+  return render_template('lista.html', datos=session['datos'])
 
 @app.route('/verificar', methods=['GET', 'POST'])
 def verificar():
+  if 'id' not in session:
+      session['id'] = 0
+  session['id'] += 1
+  
+  id = session['id']  # Asigna el valor a la variable local id
   fecha = request.form.get('fecha')
   nombre = request.form.get('nombre')
   apellido = request.form.get('apellido')
@@ -19,7 +28,7 @@ def verificar():
   seminarios_str = ', '.join(seminario)
   if 'datos' not in session:
     session['datos'] =[]
-  session['datos'].append({'fecha':fecha,'nombre':nombre,'apellido':apellido,'turno':turno,'seminario':seminarios_str})
+  session['datos'].append({'id':id,'fecha':fecha,'nombre':nombre,'apellido':apellido,'turno':turno,'seminario':seminarios_str})
   session.modified=True
   print(session['datos'])
   return redirect(url_for('index'))
@@ -29,6 +38,19 @@ def vaciar():
   # Eliminanos la sesion
   session.pop("datos", None)
   return redirect(url_for('index'))
+
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    # Lógica para editar el registro con el id correspondiente
+    return f'Editar registro con ID: {id}'
+
+@app.route('/borrar/<int:id>')
+def borrar(id):
+    # Lógica para borrar el registro con el id correspondiente
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
